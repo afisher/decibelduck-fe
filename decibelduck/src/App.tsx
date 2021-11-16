2142df28b58c4a1452439e3cf2adf49268a2adb4
Ashley Fisher <fish.ash@gmail.com>
2021-11-15 20:19:00 -0800

    button grid

--- decibelduck/src/App.tsx
+++ decibelduck/src/App.tsx
@@ -1,24 +1,16 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
+  const rows = 4;
+  const cols = 4;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="midiButtonsContainer">
+      {[...Array(rows)].map((_, row) =>
+        [...Array(cols)].map((_, col) => (
+          <button className="midiButton" key={`button-${row}-${col}`}></button>
+        ))
+      )}
     </div>
   );
 };
